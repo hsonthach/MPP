900a65b47aecd4d513dbf3fdbe28f2afe5679843
Son Thach <son.thach011011@gmail.com>
2024-09-17 13:41:51 -0500

    add sol1

--- lab8/Problem1.java
+++ lab8/Problem1.java
@@ -11,32 +11,32 @@ public class Problem1 {
 		// To understand functions as First class citizen
 		 List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
 		 // Task 1: Identify the suitable interface to read the input and print doubled
-		 changeDouble(numbers /* , Functional Interface implementation*/ );// Output: 2 4 6 8 10
-	
+		 changeDouble(numbers,(number) -> System.out.println(number*2));// Output: 2 4 6 8 10
+
 		 List<String> data = Arrays.asList("apple", "banana", "cherry");
-	     //Task 2: Take input of String and transform into Upper case   
-		 transformStrings(data /*, Functional Interface implementation*/);
+	     //Task 2: Take input of String and transform into Upper case
+		 transformStrings(data,(s)-> s.toUpperCase());// Output: APPLE BANANA CHERRY
 		 
 		 // Task 3: Check the inputs of given value is divisible by 5.
 		 List<Integer> inputs = Arrays.asList(10, 21, 12, 25, 33);
-	        printIf(inputs /*, Functional Interface implementation*/);
+		 printIf(inputs, (number) -> number % 5 == 0);// Output: 10 25
 	}
 
-	public static void changeDouble(List<Integer> numbers /*,Functional Interface action */) {
+	public static void changeDouble(List<Integer> numbers, Consumer<Integer> action) {
         for (Integer number : numbers) {
-            //action.accept(number);
+            action.accept(number);
         }
 	}
-	public static void transformStrings(List<String> list/*,Functional Interface transformer */) {
+	public static void transformStrings(List<String> list, Function<String, String> transformer) {
         for (String s : list) {
-          //  System.out.println(transformer.apply(s));
+			System.out.println(transformer.apply(s));
         }
     }
-	public static void printIf(List<Integer> numbers/*, transformer condition*/) {
+	public static void printIf(List<Integer> numbers, Predicate<Integer> condition) {
         for (Integer number : numbers) {
-           /* if (condition.test(number)) {
+           if (condition.test(number)) {
                 System.out.println(number);
-            }*/
+		   }
         }
     }
 }
